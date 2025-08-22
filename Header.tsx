df88650c71a4fb6a1e7d0e566874998da8d36be2
taikenji07-tech <taikenji07@gmail.com>
2025-08-22 15:31:15 +0800

    Update Header.tsx

--- Header.tsx
+++ Header.tsx
@@ -34,8 +34,8 @@ export const Header: React.FC<HeaderProps> = ({ score, streak, theme, onThemeTog
                 <div className="flex items-center justify-between gap-4">
                     {/* Left side: Title & Logo */}
                     <div className="flex flex-col">
-                        <a href="https://www.rhbinsurance.com.my/" target="_blank" rel="noopener noreferrer" aria-label="Brought to you by Maybank">
-                            <img src="https://i.imgur.com/YEr2agc.png" alt="Brought to you by Maybank" className="h-auto w-24" />
+                        <a href="https://www.rhbinsurance.com.my/" target="_blank" rel="noopener noreferrer" aria-label="Brought to you by RHB Insurance">
+                            <img src="https://i.imgur.com/YEr2agc.png" alt="Brought to you by RHB Insurance" className="h-auto w-24" />
                         </a>
                         <img src={logoUrl} alt="ESG Student Guide by RHB" className="mt-2 h-14" />
                     </div>
